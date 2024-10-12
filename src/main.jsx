b14e1239018059de4a8e831c5278db16f33c579f
Ayushya <ayushy52@gmail.com>
2024-10-12 14:12:24 +0530

    Fix: Navigation Issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
+import CircularProgress from '@mui/material/CircularProgress';
+import Stack from '@mui/material/Stack';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
@@ -9,15 +11,27 @@ import App from './App.jsx';
 import ThemeLoader from './theme-loader/ThemeLoader.jsx';
 
 import { store } from 'store/index.js';
-import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <ThemeLoader>
       <BrowserRouter>
-        <Toaster />
-        <App />
+        <Suspense
+          fallback={
+            <Stack
+              width="100%"
+              height="100vh"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <CircularProgress />
+            </Stack>
+          }
+        >
+          <Toaster />
+          <App />
+        </Suspense>
       </BrowserRouter>
     </ThemeLoader>
   </Provider>
