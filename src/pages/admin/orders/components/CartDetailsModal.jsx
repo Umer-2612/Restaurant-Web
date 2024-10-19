d202a7f4c343789ccd58b9acec1766b643f18027
Umer Karachiwala <karachiwalaumer2612@gmail.com>
2024-10-19 20:03:19 +0530

    fix: 1. print function button issue

--- src/pages/admin/orders/components/CartDetailsModal.jsx
+++ src/pages/admin/orders/components/CartDetailsModal.jsx
@@ -1,158 +1,141 @@
 import React from 'react';
-
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import dayjs from 'dayjs';
 import { PropTypes } from 'prop-types';
-
 import DialogLayout from 'layouts/DialogLayout';
 import './../index.css';
 
 const CartDetailsModal = ({ menuProps, handleMenuModalClose }) => {
-  // Function to handle printing the order details
   const cartDetails = menuProps?.cartDetails?.cart;
-  console.log(cartDetails);
+
   const handlePrint = () => {
-    const newWindow = window.open();
+    const newWindow = window.open('', '_blank');
+
+    if (!newWindow) {
+      alert('Please allow popups for this website');
+      return;
+    }
+
     newWindow.document.write(`
-   <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant Receipt</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        .receipt-container {
-            max-width: 600px;
-            margin: 30px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        .logo {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .logo img {
-            max-width: 150px;
-        }
-        .restaurant-name {
-            text-align: center;
-            font-size: 1.5em;
-            margin-bottom: 20px;
-        }
-        .details, .footer {
-            text-align: center;
-            margin-top: 20px;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-        table, th, td {
-            border: 1px solid #ddd;
-        }
-        th, td {
-            padding: 8px;
-            text-align: left;
-        }
-        .total {
-            font-weight: bold;
-        }
-        .thankyou {
-            margin-top: 20px;
-            text-align: center;
-            font-weight: bold;
-            font-size: 1.2em;
-        }
-    </style>
-</head>
-<body>
-
-    <div class="receipt-container">
-        <!-- Logo Section -->
-        <div class="logo">
-            <img src="logo.png" alt="Restaurant Logo">
-        </div>
-
-        <!-- Restaurant Name -->
-        <div class="restaurant-name">
-            Punjabi Touch Indian Restaurant
-        </div>
-
-        <!-- Receipt Details -->
-        <div class="details">
-            <p>Receipt ID: ${menuProps?.cartDetails?._id}</p>
-            <p>Date: ${menuProps?.cartDetails?.createdAt ? dayjs(menuProps?.cartDetails?.createdAt).format('MMM DD YYYY - hh:mm A') : '-'}</p>
-        </div>
-
-        <!-- Product Table -->
-        <table>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                    ${cartDetails
-                      ?.map((cart) =>
-                        cart
-                          ? `<tr key="${cart?.item?._id}">
-              <td>${cart?.item?.itemName}</td>
-              <td>${cart?.quantity}</td>
-              <td>${cart?.item?.itemPrice}</td>
-            </tr>`
-                          : ''
-                      )
-                      .join('')}
-            </tbody>
-        </table>
-
-        <!-- Total and Tax -->
-        <table>
-            <tbody>
-                <tr class="total">
-                    <td>Total</td>
-                    <td>Total: $${menuProps?.cartDetails?.totalPrice?.toFixed(2) || 'Payment No Completed'}</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Footer Section -->
-        <div class="thankyou">
-            Thank you for choosing us!
-        </div>
-        <div class="footer">
-            356 Middle Rd, Greenbank QLD 4124, Australia
-        </div>
-    </div>
-
-</body>
-</html>
-            `);
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Restaurant Receipt</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  margin: 0;
+                  padding: 0;
+                  background-color: #f4f4f4;
+              }
+              .receipt-container {
+                  max-width: 600px;
+                  margin: 30px auto;
+                  padding: 20px;
+                  background-color: #fff;
+                  border-radius: 10px;
+                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              }
+              .logo {
+                  text-align: center;
+                  margin-bottom: 20px;
+              }
+              .logo img {
+                  max-width: 150px;
+              }
+              .restaurant-name {
+                  text-align: center;
+                  font-size: 1.5em;
+                  margin-bottom: 20px;
+              }
+              .details, .footer {
+                  text-align: center;
+                  margin-top: 20px;
+              }
+              table {
+                  width: 100%;
+                  border-collapse: collapse;
+                  margin-top: 20px;
+              }
+              table, th, td {
+                  border: 1px solid #ddd;
+              }
+              th, td {
+                  padding: 8px;
+                  text-align: left;
+              }
+              .total {
+                  font-weight: bold;
+              }
+              .thankyou {
+                  margin-top: 20px;
+                  text-align: center;
+                  font-weight: bold;
+                  font-size: 1.2em;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="receipt-container">
+              <div class="logo">
+                  <img src="logo.png" alt="Restaurant Logo">
+              </div>
+              <div class="restaurant-name">Punjabi Touch Indian Restaurant</div>
+              <div class="details">
+                  <p>Receipt ID: ${menuProps?.cartDetails?._id}</p>
+                  <p>Date: ${menuProps?.cartDetails?.createdAt ? dayjs(menuProps?.cartDetails?.createdAt).format('MMM DD YYYY - hh:mm A') : '-'}</p>
+              </div>
+              <table>
+                  <thead>
+                      <tr>
+                          <th>Product</th>
+                          <th>Quantity</th>
+                          <th>Price</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      ${cartDetails
+                        ?.map((cart) =>
+                          cart
+                            ? `<tr key="${cart?.item?._id}">
+                                <td>${cart?.item?.itemName}</td>
+                                <td>${cart?.quantity}</td>
+                                <td>${cart?.item?.itemPrice}</td>
+                              </tr>`
+                            : ''
+                        )
+                        .join('')}
+                  </tbody>
+              </table>
+              <table>
+                  <tbody>
+                      <tr class="total">
+                          <td>Total</td>
+                          <td>Total: $${menuProps?.cartDetails?.totalPrice?.toFixed(2) || 'Payment Not Completed'}</td>
+                      </tr>
+                  </tbody>
+              </table>
+              <div class="thankyou">Thank you for choosing us!</div>
+              <div class="footer">356 Middle Rd, Greenbank QLD 4124, Australia</div>
+          </div>
+      </body>
+      </html>
+    `);
     newWindow.document.close();
     newWindow.focus();
     newWindow.print();
-    newWindow.close();
+    newWindow.onafterprint = function () {
+      newWindow.close();
+    };
   };
 
-  console.log(menuProps);
   return (
     <DialogLayout
-      handleCloseDialog={() => {
-        handleMenuModalClose();
-      }}
+      handleCloseDialog={handleMenuModalClose}
       dialogActions={
         <Stack direction="row" spacing={2}>
           <Button
