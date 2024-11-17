46cf0e5ae42f63ba639f6955956ebcc098e082c9
shivkadiwala20 <shivkadiwala11@gmail.com>
2024-11-17 22:29:46 +0530

    Fixed socket issue in orders

--- src/store/apis/orders.js
+++ src/store/apis/orders.js
@@ -7,33 +7,32 @@ const categoriesApi = createApiInstance.injectEndpoints({
   endpoints: (build) => ({
     getAllOrders: build.query({
       query: (query) => `/orders${queryParamsBuilder(query)}`,
-    }),
+      async onCacheEntryAdded(
+        args,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        try {
+          await cacheDataLoaded;
 
-    async onCacheEntryAdded(
-      args,
-      { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
-    ) {
-      try {
-        await cacheDataLoaded;
+          const socket = getSocketInstance();
 
-        const socket = getSocketInstance();
+          if (socket) {
+            const listener = (data) => {
+              updateCachedData((draft) => {
+                draft.data.unshift(data);
+              });
+            };
 
-        if (socket) {
-          const listener = (data) => {
-            updateCachedData((draft) => {
-              draft.data.unshift(data);
-            });
-          };
+            socket.on('orders', listener);
 
-          socket.on('orders', listener);
-
-          await cacheEntryRemoved;
-          socket.off('orders', listener);
+            await cacheEntryRemoved;
+            socket.off('orders', listener);
+          }
+        } catch (err) {
+          console.error('Error in onCacheEntryAdded:', err);
         }
-      } catch (err) {
-        console.error('Error in onCacheEntryAdded:', err);
-      }
-    },
+      },
+    }),
   }),
 });
 export const { useGetAllOrdersQuery } = categoriesApi;
